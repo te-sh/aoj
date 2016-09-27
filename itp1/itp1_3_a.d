aa62ae1c429abccececc06f81f4dd254f447cba8
te-sh <admire.japan@gmail.com>
2016-09-27 15:37:02 +0900

    add itp1_3

---
+++ itp1/itp1_3_a.d
@@ -0,0 +1,7 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+void main()
+{
+  foreach (_; 0..1000)
+    writeln("Hello World");
+}
---
+++ itp1/itp1_3_b.d
@@ -0,0 +1,10 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+void main()
+{
+  for (auto i = 1; ; ++i) {
+    auto x = readln.chomp.to!int;
+    if (x == 0) break;
+    writefln("Case %d: %d", i, x);
+  }
+}
