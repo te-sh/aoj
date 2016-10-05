69085bd16e8127daaf8cd98649e45b854bd17718
te-sh <admire.japan@gmail.com>
2016-10-05 16:00:13 +0900

    add alds1_12

---
+++ alds/alds1_12_a.d
@@ -0,0 +1,57 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+import std.typecons;
+
+void main()
+{
+  auto n = readln.chomp.to!size_t;
+
+  auto gij = new int[][](n, n);
+  foreach (i; n.iota)
+    gij[i] = readln.split.to!(int[]);
+
+  edge[] ei;
+  foreach (i; 0..n-1)
+    foreach (j; i+1..n)
+      if (gij[i][j] >= 0)
+        ei ~= edge(i, j, gij[i][j]);
+  ei.sort!("a.w < b.w");
+
+  auto r = 0;
+  auto uf = new UnionFind!size_t(n);
+  foreach (e; ei) {
+    if (uf.find(e.u) != uf.find(e.v)) {
+      uf.unite(e.u, e.v);
+      r += e.w;
+    }
+  }
+
+  writeln(r);
+}
+
+alias Tuple!(size_t, "u", size_t, "v", int, "w") edge;
+
+class UnionFind(T)
+{
+  T[] p; // parent
+  T s; // sentinel
+
+  this(T n) {
+    p = new T[](n);
+    s = n + 1;
+    p[] = s;
+  }
+
+  T find(T i) {
+    if (p[i] == s) {
+      return i;
+    } else {
+      p[i] = find(p[i]);
+      return p[i];
+    }
+  }
+
+  void unite(T i, T j) {
+    auto pi = find(i), pj = find(j);
+    if (pi != pj) p[pj] = pi;
+  }
+}
---
+++ alds/alds1_12_b.d
@@ -0,0 +1,49 @@
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+import std.container, std.typecons;
+
+void main()
+{
+  auto n = readln.chomp.to!size_t;
+  auto eij = new DList!edge[](n);
+
+  foreach (_1; n.iota) {
+    auto rd = readln.splitter;
+    auto i = rd.front.to!size_t; rd.popFront;
+    auto k = rd.front.to!size_t; rd.popFront;
+    foreach (_2; k.iota) {
+      auto v = rd.front.to!size_t; rd.popFront;
+      auto c = rd.front.to!int; rd.popFront;
+      eij[i].insertBack(edge(v, c));
+    }
+  }
+
+  auto wi = dijkstra(n, eij, 0);
+  foreach (i, w; wi)
+    writefln("%d %d", i, w);
+}
+
+alias Tuple!(size_t, "v", int, "w") edge;
+
+int[] dijkstra(size_t n, DList!edge[] eij, size_t s)
+{
+  auto wi = new int[](n);
+  wi[] = -1;
+
+  auto bi = new edge[](1_000_000);
+  bi[0] = edge(s, 0);
+  auto hi = bi.heapify!("a.w > b.w")(1);
+
+  while (!hi.empty) {
+    auto es = hi.front; hi.removeFront;
+    if (wi[es.v] >= 0) continue;
+    wi[es.v] = es.w;
+
+    foreach (et; eij[es.v]) {
+      if (wi[et.v] >= 0) continue;
+      wi[et.v] = min(wi[et.v], es.w + et.w);
+      hi.insert(edge(et.v, es.w + et.w));
+    }
+  }
+
+  return wi;
+}
